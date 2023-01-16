b055b9442e30ca3a166e2e840373210931826efa
Washington Fernandes de Barros <w_as@yahoo.com>
2023-01-16 11:03:03 -0300

    Nova atualização

---
+++ cplusplus/aula04-operadores.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	float n1,n2,resultado;
+	n1=10;
+	n2=20;
+	resultado=n1/n2;
+	cout<<resultado<<endl;
+	return 0;
+}
