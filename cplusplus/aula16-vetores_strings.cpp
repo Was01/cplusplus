93eb5812c97b3c0eee62bae73a64e0b191a0a971
Washington Fernandes de Barros <w_as@yahoo.com>
2023-01-24 13:54:09 -0300

    Nova atualização

---
+++ cplusplus/aula16-vetores_strings.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+void inverte(char nome[]){
+	int tam,i;
+	for(tam=0;nome[tam];tam++){
+		
+	}
+    for(i=tam-1;i>=0;i--)
+    	cout<<nome[i];
+}
+
+int main(){
+	char nome[]="Maria";
+	inverte(nome);
+	return 0;
+}
