d8d4ba7bc8d52a907ff8447c3214d0f91e7719cc
Washington Fernandes de Barros <w_as@yahoo.com>
2023-04-29 21:18:10 -0300

    nova atualização

---
+++ cplusplus/cfb/aula18.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int vet[5];
+	int i;
+	vet[0]=10;
+	vet[1]=20;
+	vet[2]=30;
+	vet[3]=40;
+	vet[4]=50;
+	for(i=0;i<5;i++){
+		cout<<vet[i]<<endl;
+	}
+	
+	return 0;
+	
+}
