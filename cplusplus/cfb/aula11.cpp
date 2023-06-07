e3ced4c052a9888d34775d9e5ec7ba9611d9160f
Washington Fernandes de Barros <w_as@yahoo.com>
2023-06-07 20:00:27 -0300

    Nova atualização

--- cplusplus/cfb/aula11.cpp
+++ cplusplus/cfb/aula11.cpp
@@ -8,7 +8,7 @@ int main(int argc, char** argv)
 	int num;
 	cout<<"Digite um número inteiro: ";
 	cin>>num;
-	if(num>=4 && num <=7){
+	if((num>=4 && num <7)||(num>10&&num<=13)){
 		cout<<"\nNumero no intervalor correto!!!\n";
 	}else{
 			cout<<"\nNumero fora do intervalo!!!\n";
